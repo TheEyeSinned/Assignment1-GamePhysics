0a14c97ee9ceea9040d2acf455a7c70d4f8907c6
Eisen <mister.eisen@gmail.com>
2020-10-08 11:46:43 -0400

    Added initial files

---
+++ src/Target.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef __TARGET__
+#define __TARGET__
+
+#include "DisplayObject.h"
+
+class Target final : public DisplayObject {
+public:
+	Target();
+	~Target();
+
+	// Inherited via GameObject
+	virtual void draw() override;
+	virtual void update() override;
+	virtual void clean() override;
+	bool isGravityEnabled = false;
+
+	glm::vec2 throwSpeed;
+	glm::vec2 throwPosition;
+
+	void doThrow();
+
+	float metersPerPixel = 10;
+private:
+	void m_move();
+	void m_checkBounds();
+	void m_reset();
+};
+
+
+#endif /* defined (__TARGET__) */
